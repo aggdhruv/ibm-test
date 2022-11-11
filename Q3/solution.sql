39cce6172e73584713d2d2a71e44585b628ca423
aggdhruv <aggdhruv@connect.hku.hk>
2022-11-12 02:12:39 +0800

    updated q3

--- Q3/solution.sql
+++ Q3/solution.sql
@@ -1,4 +1,4 @@
-select A.owner_id, O.owner_name, count(*) as article_count
-from owner O, article A
-where O.owner_id = A.owner_id
+select A.owner_id, O.owner_name, count(CAM.category_id) as category_count
+from owner O, article A, category_article_mapping CAM       
+where O.owner_id = A.owner_id and A.article_id = CAM.article_id
 group by O.owner_id
